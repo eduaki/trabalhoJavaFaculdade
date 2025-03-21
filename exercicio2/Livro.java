97518b90f2067486a310b253eb8a33644a641888
Eduardo Alves <edu.alves2003@gmail.com>
2025-03-21 10:43:05 -0300

    exercicio2 finalizado

---
+++ exercicio2/Livro.java
@@ -0,0 +1,28 @@
+package exercicio2;
+public class Livro {
+    String titulo;
+    String autor;
+    int ano;
+
+
+    Livro(String titulo, String autor, int ano){
+        this.titulo = titulo;
+        this.autor = autor;
+        this.ano = ano;
+    }
+
+    String getTitulo(){
+        return this.titulo;
+    }
+
+    void exibirInfo(){
+        System.out.println("\n-----------------------\n");
+        System.out.println("titulo: " + this.titulo);
+        System.out.println("autor: " + this.autor);
+        System.out.println("ano: " + this.ano);
+    }
+
+
+}
+
+
